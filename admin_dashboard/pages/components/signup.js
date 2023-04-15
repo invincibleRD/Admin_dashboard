76661a745dc90d491f51854985684d9a7d5b3b72
DIPENDRA KUMAR SHAH <74634983+invincibleRD@users.noreply.github.com>
2023-04-16 01:19:02 +0530

    fixing build bugs

--- admin_dashboard/pages/components/signup.js
+++ admin_dashboard/pages/components/signup.js
@@ -10,32 +10,7 @@ export default function SignUp() {
           <div className={styles.google}>Sign in with Google</div>
           <div className={styles.google}>Sign in with Apple</div>
         </div>
-        <form className={styles.formInputs}>
-          <div className={styles.title}>Enter address</div>
-          <input
-            type="email"
-            placeholder="Enter your  email"
-            name="email"
-            required
-          ></input>
-          <div className={styles.title}>Password</div>
-          <input
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            required
-          ></input>
-          <div className={`${styles.title} ${styles.forgot}`}>
-            Forgot password?
-          </div>
-          <button
-            className={styles.submitButton}
-            style={{ backgroundColor: "black" }}
-            type="submit"
-
-            required
-          >Sign In</button>
-        </form>
+        
         <div className={styles.register}>
           <div style={{ color: "black", fontSize: ".8rem" }}>
             Don't have an account?
